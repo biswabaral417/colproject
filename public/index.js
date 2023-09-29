ed6f41f3f5802ff41b457434d58e7ffd4e4b6d5c
biswabaral <131336755+biswabaral417@users.noreply.github.com>
2023-09-29 19:46:37 +0545

    idont know what i changed but it should work

--- public/index.js
+++ public/index.js
@@ -1,28 +1,16 @@
-
 let foodsArr = [];
 
-
 const fetchData = async () => {
+  try {
     const response = await fetch("/api/foods");
     const data = await response.json();
-    // console.log(data)
-    pushJsonData(data);
+    foodsArr = data[0].foods; // Store the fetched data in the 'foodsArr' variable
+    console.table(foodsArr);
+    findSeason();
+  } catch (error) {
+    console.error('Error fetching data from the server:', error);
+  }
 };
-
-const pushJsonData = (data) => {
-    data.forEach(element => {
-        foodsArr.push(element);
-    });
-};
-
-
-
-(async () => {
-    await fetchData();
-    findSeason()
-    // Call other functions that depend on foodsArr here
-
-})();
 
 
 const findSeason = () => {
@@ -71,45 +59,56 @@ const findSeason = () => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const render = (() => {
-    let i = 1;
-    const foodSection = document.querySelector(`.foods`);
-    const iRender = (item) => {
-        foodSection.innerHTML +=
-            `<div class="item">
-                    <div class="itmimg"><img src="${item.itemImgLoc}" alt=""></div>
-                    <p class="itmname">${item.itemName}</p>
-                    <p class="itemprice">${item.itemPrice}</p>
-                    <button class="atcbutton" onclick="addtocart()">add to cart</button>
-                </div>
-            `
-        i++;
-    }
+  let i = 1;
+  const foodSection = document.querySelector(`.foods`);
+  const iRender = (item) => {
+    foodSection.innerHTML +=
+      `<div class="item">
+        <div class="itmimg"><img src="${item.itemImgLoc}" alt=""></div>
+        <p class="itmname">${item.itemName}</p>
+        <p class="itemprice">${item.itemPrice}</p>
+        <button class="atcbutton" onclick="addtocart()">add to cart</button>
+      </div>`;
+    i++;
+  };
 
-    return {
-        iRender,
-
-    }
+  return {
+    iRender,
+  };
 })();
-
-
-
-
-//serch  
 
 const sbtn = document.querySelector(`.sbtn`);
 sbtn.addEventListener("click", async () => {
-    document.querySelector(`.foods`).innerHTML = "";
-    console.log("click");
-    let searchTxt = document.querySelector(`.stext`).value;
+  document.querySelector(`.foods`).innerHTML = "";
+  console.log("click");
+  let searchTxt = document.querySelector(`.stext`).value;
 
-    const response = await fetch(`/api/search?searchText=${encodeURIComponent(searchTxt)}`);
-    const filteredData = await response.json();
+  const response = await fetch(`/api/search?searchText=${encodeURIComponent(searchTxt)}`);
+  const filteredData = await response.json();
 
-    i = 1;
-    filteredData.forEach(item => {
-        render.iRender(item);
-    });
+  let i = 1; // Make sure to use 'let' for 'i' to avoid confusion with the 'i' variable in the render function
+  filteredData.forEach(item => {
+    render.iRender(item);
+  });
 
-    console.log(searchTxt);
+  console.log(searchTxt);
 });
+
+(async () => {
+  await fetchData();
+})();
--- server.js
+++ server.js
@@ -1,30 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const path = require('path');
 
 // Serve static files (HTML, CSS, and client-side JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Mock data for foods (replace this with your actual data source or database)
-const foodsData = require('./public/foods.json');
 
-// Endpoint to send the foods data to the frontend
-app.get('/api/foods', (req, res) => {
-  res.json(foodsData.foods);
+
+// data for foods database)
+const mongoURI = 'mongodb://localhost:27017/foodsDB';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// Endpoint to handle search requests
-app.get('/api/search', (req, res) => {
-  const { searchText } = req.query;
 
-  const filteredFoods = foodsData.foods.filter((item) =>
-    item.itemName.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-  res.json(filteredFoods);
+//check connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database!');
 });
 
-const PORT = 3000; // Change this to your preferred port
+
+// Define the schema for the 'fooditems' collection
+const foodSchema = new mongoose.Schema({
+  itemName: String,
+  itemPrice: String,
+  itemImgLoc: String,
+  rSeasons: String,
+});
+
+// Create the 'Food' model based on the 'foodSchema'
+const Food = mongoose.model('foods', foodSchema);
+
+// Add a new route to get all food items from the 'fooditems' collection
+app.get('/api/foods', async (req, res) => {
+  try {
+    const allFoods = await Food.find({});
+    res.json(allFoods);
+  } catch (error) {
+    console.error('Error fetching data from MongoDB:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
+const PORT = 3005; // Change this to your preferred port
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
